baaa2a98e6503eb954a216e13bb69e810815a2ac
Mark Tinderholt <mark_tinderholt@persistent.co.in>
2020-06-04 09:19:55 -0400

    premium plan

---
+++ plan/premium/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kind                = "elastic"
+
+  sku {
+    tier = "ElasticPremium"
+    size = "EP2"
+  }
+
+  pre_warmed_instance_count = 1
+
+  tags = {
+    app = var.app_name
+    env = var.env_name
+  }
+
+}
+
+module "func_storage" {
+  
+  source                = "github.com/markti/tf_azure_storage/blob/account"
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  app_name              = var.app_name
+  env_name              = var.env_name
+  name                  = "func"
+
+  type                  = var.storage_type
+
+}
